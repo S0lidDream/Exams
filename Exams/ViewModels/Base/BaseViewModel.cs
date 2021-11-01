597700859e1664f32f1c650bf9c0e2cf0c7b36e7
Artem Pshichko <brennend3956@gmail.com>
2021-11-01 19:41:39 +0600

    Создаем базовый класс для ViewModel

---
+++ Exams/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exams.ViewModels.Base
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if(Equals(field, value))
+            {
+                return false;
+            }
+            else
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+        }
+    }
+}
